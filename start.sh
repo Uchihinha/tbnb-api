f42429480c798db12e8bd6cfde161ec649cbc2b4
Gabriel Jorge <gabriel.jorge.pereira@outlook.com>
2021-06-28 14:51:24 -0300

    chore: updating startup script file

--- start.sh
+++ start.sh
@@ -2,27 +2,24 @@
 echo "Aliasing $FRAMEWORK"
 sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
-# cria o arquivo de configuração de ambiente
+# copy the env example file to .env
 cp .env.example .env
 
-# instala as dependências do projeto
+# install dependencies
 composer install
 
-# cria e configura o banco para os testes
+# configure test database
 sed -i '12s/.*/DB_HOST=db-test/' .env
 php /var/www/app/artisan config:cache
 php /var/www/app/artisan migrate
 
-# cria e configura o banco principal
+# configure main database
 sed -i '12s/.*/DB_HOST=db/' .env
 php /var/www/app/artisan config:cache
 php /var/www/app/artisan migrate --seed
 
-# liga o fpm, serviço que vai rodar o php no server
+# set fpm service
 nohup /usr/sbin/php-fpm -y /etc/php7/php-fpm.conf -F -O 2>&1 &
 
-# liga a fila do Laravel para ficar escutando as requisições da aplicação
-nohup php /var/www/app/artisan queue:work --verbose --tries=3 --timeout=90 &
-
-# liga o servidor nginx
+# start nginx server
 nginx
